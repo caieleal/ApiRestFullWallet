f2b2ba0b22bb8613ebf0dbaa3f143a46c67322fc
Caiê Leal <caieleals@gmail.com>
2020-04-27 16:20:51 -0300

    Criando as camadas de serviços

---
+++ src/main/java/com/wallet/service/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.wallet.service;
+
+import com.wallet.entity.User;
+import com.wallet.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmailEquals(email);
+    }
+}
