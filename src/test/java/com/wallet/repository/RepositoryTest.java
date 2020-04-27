56510876e8ace13a804da85adae5caedccee2d1f
Caiê Leal <caieleals@gmail.com>
2020-04-27 14:20:40 -0300

    criando repositório de usuário, e alguns testes de CRUD

---
+++ src/test/java/com/wallet/repository/RepositoryTest.java
@@ -0,0 +1,56 @@
+package com.wallet.repository;
+
+import com.wallet.entity.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RepositoryTest {
+
+    private static final String EMAIL = "teste@teste.com";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void init(){
+        User user = new User();
+        user.setName("test init");
+        user.setPassword("senha34561");
+        user.setEmail(EMAIL);
+        userRepository.save(user);
+
+    }
+    @After
+    public void finish(){
+        userRepository.deleteAll();
+
+    }
+
+    @Test
+    public void testSave(){
+        User user = new User();
+        user.setName("teste");
+        user.setPassword("12345");
+        user.setEmail("teste@teste.com");
+
+        User response = userRepository.save(user);
+        assertNotNull(response);
+    }
+    @Test
+    public void testFindByEmail(){
+        Optional<User> response = userRepository.findByEmailEquals(EMAIL);
+        assertTrue(response.isPresent());
+        assertEquals(response.get().getEmail(), EMAIL);
+    }
+}
